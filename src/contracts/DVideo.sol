615d16f3935923ff60d25f35cb1bc2a7e5a0e2a6
promehedi <mmhasan563@gmail.com>
2022-01-11 04:54:30 +0600

    Finishing DVideo Contract

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -13,21 +13,18 @@ contract DVideo {
     address author;
   }
 
-
   //Create Event
   event VideoUploaded(uint id, string hash, string title, address author);
 
-  //Constructor
-  constructor() {
-  }
-
   function uploadVideo(string memory _videoHash, string memory _title) public {
     // Make sure the video hash exists
+    require(bytes(_videoHash).length > 0);
 
     // Make sure video title exists
+    require(bytes(_title).length > 0);
 
     // Make sure uploader address exists
-
+    require(msg.sender != address(0));
 
     // Increment video id
     videoCount++;

539e221ebccd400a6fd0817d3c8c440a431df824
promehedi <mmhasan563@gmail.com>
2022-01-11 04:46:18 +0600

    Model the video

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,17 +1,23 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  mapping(uint => Video) public videos;
 
-  //Create Struct
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address author;
+  }
 
 
   //Create Event
 
 
-  constructor() public {
+  constructor() {
   }
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
